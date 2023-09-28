94888e29895b936f5a8ab3976c57e1c8cd36f606
Giovanni Hislop <89380684+gmhislop@users.noreply.github.com>
2023-09-28 13:12:41 +0200

    [Improvement] - Change loading image

--- src/components/loader.tsx
+++ src/components/loader.tsx
@@ -4,7 +4,7 @@ export const Loader = () => {
   return (
     <div className="flex flex-col items-center justify-center h-full gap-y">
       <div className="relative w-10 h-10 animate-spin">
-        <Image src="/loading.png" fill alt="Logo" />
+        <Image src="/loading-2.png" fill alt="Logo" />
       </div>
       <p className="text-sm text-muted-foreground">Guru is thinking...</p>
     </div>
