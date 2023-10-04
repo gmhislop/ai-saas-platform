fdfeb708ba1ddf5896ae7e623457b46bca0483b6
Giovanni Hislop <89380684+gmhislop@users.noreply.github.com>
2023-10-04 14:02:39 +0200

    [feat] - Implement premium section

---
+++ src/components/free-counter.tsx
@@ -0,0 +1,43 @@
+'use client';
+
+import React from 'react';
+import { Card, CardContent } from '@/components/ui/card';
+import { MAX_FREE_COUNTS } from '@/constants';
+import { Progress } from '@/components/ui/progress';
+import { Button } from './ui/button';
+import { Zap } from 'lucide-react';
+
+type FreeCounterProps = {
+  apiLimitCount: number;
+};
+
+export const FreeCounter = ({ apiLimitCount = 0 }: FreeCounterProps) => {
+  const [mounted, setmounted] = React.useState(false);
+
+  React.useEffect(() => {
+    setmounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
+
+  return (
+    <div className="px-3">
+      <Card className="border-0 bg-white/10">
+        <CardContent className="py-6">
+          <div className="mb-4 space-y-2 text-sm text-center text-white">
+            <p>
+              {apiLimitCount} / {MAX_FREE_COUNTS} Free Generations
+            </p>
+            <Progress className="h-3" value={(apiLimitCount / MAX_FREE_COUNTS) * 100} />
+          </div>
+          <Button variant={'premium'} className="w-full">
+            Upgrade
+            <Zap fill="white" className="w-4 h-4 ml-2" />
+          </Button>
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
