03f265ce72d3a33245580ca4ba78e8f12f59808c
Giovanni Hislop <89380684+gmhislop@users.noreply.github.com>
2024-02-06 20:57:04 +0100

    [Improvement] - Close button color

--- src/app/api/stripe/route.ts
+++ src/app/api/stripe/route.ts
@@ -7,61 +7,60 @@ import { absoluteUrl } from '@/lib/utils';
 const settingsUrl = absoluteUrl('/settings');
 
 export async function GET() {
-    try {
-        const { userId } = auth();
-        const user = await currentUser();
+  try {
+    const { userId } = auth();
+    const user = await currentUser();
 
-        if (!user || !userId) {
-            return new NextResponse('Unauthorized', { status: 401 });
-        }
-
-        const userSubscription = await prismadb.userSubscription.findFirst({
-            where: {
-                userId,
-            },
-        });
-
-        if (userSubscription && userSubscription.stripeCustomerId) {
-            const stripeSession = await stripe.billingPortal.sessions.create({
-                customer: userSubscription.stripeCustomerId,
-                return_url: settingsUrl,
-            });
-
-            return new NextResponse(JSON.stringify({ url: stripeSession.url }));
-        }
-        const stripeSession = await stripe.checkout.sessions.create({
-            success_url: settingsUrl,
-            cancel_url: settingsUrl,
-            payment_method_types: ['card'],
-            mode: 'subscription',
-            billing_address_collection: 'auto',
-            customer_email: user.emailAddresses[0].emailAddress,
-            line_items: [
-                {
-                    price_data: {
-                        currency: 'USD',
-                        product_data: {
-                            name: 'Guru Pro',
-                            description: 'Unlimited AI Generations',
-                        },
-                        unit_amount: 2000,
-                        recurring: {
-                            interval: 'month',
-                        },
-                    },
-                    quantity: 1,
-                },
-            ],
-            // This is the URL to which Stripe will send a POST request with the customer’s ID after the customer clicks the link in the email.
-            metadata: {
-                userId,
-            },
-        });
-
-        return new NextResponse(JSON.stringify({ url: stripeSession.url }));
-
-    } catch (error) {
-        console.error('STRIPE_ERROR', error);
-        return new NextResponse('Internal server error', { status: 500 });
+    if (!user || !userId) {
+      return new NextResponse('Unauthorized', { status: 401 });
     }
+
+    const userSubscription = await prismadb.userSubscription.findFirst({
+      where: {
+        userId,
+      },
+    });
+
+    if (userSubscription?.stripeCustomerId) {
+      const stripeSession = await stripe.billingPortal.sessions.create({
+        customer: userSubscription.stripeCustomerId,
+        return_url: settingsUrl,
+      });
+
+      return new NextResponse(JSON.stringify({ url: stripeSession.url }));
+    }
+    const stripeSession = await stripe.checkout.sessions.create({
+      success_url: settingsUrl,
+      cancel_url: settingsUrl,
+      payment_method_types: ['card'],
+      mode: 'subscription',
+      billing_address_collection: 'auto',
+      customer_email: user.emailAddresses[0].emailAddress,
+      line_items: [
+        {
+          price_data: {
+            currency: 'USD',
+            product_data: {
+              name: 'Guru Pro',
+              description: 'Unlimited AI Generations',
+            },
+            unit_amount: 2000,
+            recurring: {
+              interval: 'month',
+            },
+          },
+          quantity: 1,
+        },
+      ],
+      // This is the URL to which Stripe will send a POST request with the customer’s ID after the customer clicks the link in the email.
+      metadata: {
+        userId,
+      },
+    });
+
+    return new NextResponse(JSON.stringify({ url: stripeSession.url }));
+  } catch (error) {
+    console.error('STRIPE_ERROR', error);
+    return new NextResponse('Internal server error', { status: 500 });
+  }
 }
